25fa3f9d57a9520d932c063edb978e4bb4db26f8
Ramkumar-M003 <mohanramr56@gmail.com>
2025-07-30 14:37:15 +0530

    Create Project1.java

---
+++ Project1.java
@@ -0,0 +1,14 @@
+package sample;
+public class Project1
+{
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i=1;i<=1;i++)
+		{
+			sum += i;
+		}
+		System.out.println("sum="+sum);
+	}
+}
+	
+    
